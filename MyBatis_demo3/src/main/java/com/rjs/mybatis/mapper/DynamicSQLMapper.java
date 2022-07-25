1fe94c682d755476dd3e1fb57348c4bd32c9b4ae
软件手 <471583744@qq.com>
2022-07-25 09:10:14 +0800

    SQL连接 完

--- MyBatis_demo3/src/main/java/com/rjs/mybatis/mapper/DynamicSQLMapper.java
+++ MyBatis_demo3/src/main/java/com/rjs/mybatis/mapper/DynamicSQLMapper.java
@@ -1,6 +1,7 @@
 package com.rjs.mybatis.mapper;
 
 import com.rjs.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -14,4 +15,12 @@ public interface DynamicSQLMapper {
     // 多条件查询
     List<Emp> getEmpByCondition(Emp emp);
 
+    // 测试choose\when\otherwise
+    List<Emp> getEmpByChoose(Emp emp);
+
+    // 通过数组实现批量删除
+    int deleteMoreByArray(@Param("eids") Integer[] eids );
+
+    // 通过list集合实现批量添加
+    int insertMoreByList(@Param("emps") List<Emp> emps);
 }
